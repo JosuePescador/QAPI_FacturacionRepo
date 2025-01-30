dcd90a0cc1874eef1b7c92d6b5ecd19b97467462
Alexander Guerra <alexander.guerra@nuvu.cc>
2025-01-30 14:43:36 -0500

    Implementación del modelo y servicio de facturación masiva

--- src/main/java/cc/nuvu/qapi/controller/FacturacionMasivaController.java
+++ src/main/java/cc/nuvu/qapi/controller/FacturacionMasivaController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/facturacion")
+@RequestMapping("/api")
 public class FacturacionMasivaController {
 
     @Autowired
