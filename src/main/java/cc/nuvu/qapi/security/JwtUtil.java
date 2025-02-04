193d4efd76a8b7216bc3abccc5ecaed07fa3fc0b
Sebastian Vidal <sebastian.vidal@nuvu.cc>
2025-02-04 11:56:45 -0500

    Update en fix error

--- src/main/java/cc/nuvu/qapi/security/JwtUtil.java
+++ src/main/java/cc/nuvu/qapi/security/JwtUtil.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
