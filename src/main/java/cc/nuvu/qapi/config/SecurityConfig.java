87fc01007d76d2258f6e475e7b5b2c090b9778dc
Sebastian Vidal <sebastian.vidal@nuvu.cc>
2025-02-04 16:34:21 -0500

    Update SecurityConfig

--- src/main/java/cc/nuvu/qapi/config/SecurityConfig.java
+++ src/main/java/cc/nuvu/qapi/config/SecurityConfig.java
@@ -3,14 +3,16 @@ package cc.nuvu.qapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import cc.nuvu.qapi.security.JwtAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -23,18 +25,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/status", "/facturacion-masiva/**").permitAll()
+                .anyRequest().authenticated()
+            )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-     // Ignore selected URIs from security checks
+    // Este bean deshabilita la configuración automática del UserDetailsService
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-       // Ignore static directories from Security Filter Chain
-       return web -> web.ignoring().requestMatchers( "/","/status");
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            throw new UsernameNotFoundException(
+                "No user found");
+        };
     }
 }
 
