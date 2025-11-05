60a10c92a2531862208b5789277cc6f131e76667
alejandro <uscateguia.l@javeriana.edu.co>
2025-11-05 00:14:07 -0500

    feature: integración con dynamo terminada

--- src/main/java/cc/nuvu/qapi/service/ProcesarRequestService.java
+++ src/main/java/cc/nuvu/qapi/service/ProcesarRequestService.java
@@ -2,23 +2,22 @@ package cc.nuvu.qapi.service;
 
 import cc.nuvu.qapi.presentation.dto.Request;
 import cc.nuvu.qapi.utils.SqsSender;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
+import cc.nuvu.qapi.infraestructure.dynamoDB.service.InfoRequestService;
+
 @Service
+@RequiredArgsConstructor
 public class ProcesarRequestService {
 
-    private final S3Service s3service;
     private final SqsSender sqsSender;
+    private final InfoRequestService infoRequestService;
 
-    public ProcesarRequestService(S3Service s3service, SqsSender sqsSender) {
-        this.s3service = s3service;
-        this.sqsSender = sqsSender;
-    }
-
-    public String recibirYProcesarRequest(Request request){
+    public String recibirYProcesarRequest(Request request) {
         String id_mensaje = sqsSender.enviarPedido(request);
+        infoRequestService.guardar(id_mensaje, request);
         return id_mensaje;
     }
 }
-
--- src/main/java/cc/nuvu/qapi/service/S3Service.java
+++ src/main/java/cc/nuvu/qapi/service/S3Service.java
@@ -1,52 +1,52 @@
-package cc.nuvu.qapi.service;
+// package cc.nuvu.qapi.service;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+// import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+// import software.amazon.awssdk.core.sync.RequestBody;
+// import software.amazon.awssdk.regions.Region;
+// import software.amazon.awssdk.services.s3.S3Client;
+// import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Service;
+// import java.nio.charset.StandardCharsets;
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.time.format.DateTimeFormatter;
 
-@Service
-public class S3Service {
+// @Service
+// public class S3Service {
 
-    private S3Client s3Client;
+//     private S3Client s3Client;
 
-    @Value("${aws.s3.bucketname}")
-    private String bucketName;
+//     @Value("${aws.s3.bucketname}")
+//     private String bucketName;
 
-    public S3Service() {
-        this.s3Client = S3Client.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-    }
+//     public S3Service() {
+//         this.s3Client = S3Client.builder()
+//                 .region(Region.US_EAST_1)
+//                 .credentialsProvider(DefaultCredentialsProvider.create())
+//                 .build();
+//     }
 
-    public void uploadJson(String json, String tipo, String messageId) {
+//     public void uploadJson(String json, String tipo, String messageId) {
 
-        String fecha = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        LocalDateTime now = LocalDateTime.now();
-        String timestamp = now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+//         String fecha = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+//         LocalDateTime now = LocalDateTime.now();
+//         String timestamp = now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
 
-        // El ID ya viene de SQS, aseguramos que es el correcto
-        String fileName = timestamp + "-" + messageId + "-" + tipo + ".json";
-        String s3Key = "requests/" + fecha + "/" + fileName;
+//         // El ID ya viene de SQS, aseguramos que es el correcto
+//         String fileName = timestamp + "-" + messageId + "-" + tipo + ".json";
+//         String s3Key = "requests/" + fecha + "/" + fileName;
 
-        // Subir a S3
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(s3Key)
-                .contentType("application/json")
-                .build();
+//         // Subir a S3
+//         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+//                 .bucket(bucketName)
+//                 .key(s3Key)
+//                 .contentType("application/json")
+//                 .build();
 
-        s3Client.putObject(putObjectRequest, RequestBody.fromString(json, StandardCharsets.UTF_8));
+//         s3Client.putObject(putObjectRequest, RequestBody.fromString(json, StandardCharsets.UTF_8));
 
-        System.out.println("Archivo guardado en S3: " + s3Key);
-    }
-}
+//         System.out.println("Archivo guardado en S3: " + s3Key);
+//     }
+// }
